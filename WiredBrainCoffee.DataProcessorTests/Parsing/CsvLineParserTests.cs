96ee312fa37e1f520f6a5c087f88f6acc1fdf877
Andrei Corbu <andrei_corbu@waters.com>
2022-12-27 16:09:54 +0200

    Added files

---
+++ WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
@@ -0,0 +1,49 @@
+﻿namespace WiredBrainCoffee.DataProcessor.Parsing;
+
+public class CsvLineParserTests
+{
+    [Fact]
+    public void ShouldParseValidLine()
+    {
+        // Arrange
+        string[] csvLines = new[] { "Cappuccino;2022-10-27T08:06:04" };
+
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Single(machineDataItems);
+        Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
+        Assert.Equal(new DateTime(2022, 10, 27, 8, 6, 4), machineDataItems[0].CreatedAt);
+    }
+
+    [Fact]
+    public void ShouldSkipEmptyLines()
+    {
+        // Arrange
+        string[] csvLines = new[] { "", "  " };
+
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Empty(machineDataItems);
+    }
+
+    [InlineData("Cappuccino", "Invalid csv line")]
+    [InlineData("Cappuccino;InvalidDateTime", "Invalid datetime in csv line")]
+    [Theory]
+    public void ShouldThrowExceptionForInvalidLine(string csvLine,
+        string expectedMessagePrefix)
+    {
+        // Arrange
+        var csvLines = new[] { csvLine };
+
+        // Act and Assert
+        var exception = Assert.Throws<Exception>(() => CsvLineParser.Parse(csvLines));
+
+        Assert.Equal($"{expectedMessagePrefix}: {csvLine}", exception.Message);
+    }
+}
